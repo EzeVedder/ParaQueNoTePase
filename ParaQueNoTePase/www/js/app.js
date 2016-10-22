17c5d6a6c10f87560558b5f012125ab108a9ca82
rolo <teccompu2011@gmail.com>
2016-10-22 10:17:33 -0300

    Merge https://github.com/octaviovillegas/ParaQueNoTePase
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- ParaQueNoTePase/www/js/app.js
+++ ParaQueNoTePase/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','login.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -46,6 +46,17 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       views: {
         'menuContent': {
           templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.login', {
+      url: '/login',
+      cache:false,
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/login.html'
+          ,
+          controller: 'controlLogin'
         }
       }
     })
