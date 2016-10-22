c946bcbb62f85099bad23062f93333c504865e73
rolo <teccompu2011@gmail.com>
2016-10-22 11:27:13 -0300

    rolo mensajes botones

--- ParaQueNoTePase/www/js/controladoresAlarma.js
+++ ParaQueNoTePase/www/js/controladoresAlarma.js
@@ -2,5 +2,15 @@ angular.module('alarma.controllers', [])
 
 .controller('controlAlarmas', function($scope) {
  
+$scope.violenciaDeGenero=function(){
+	alert("violencia de genero");
+}
+$scope.Accidente=function(){
+	alert("accidente");
+}
+
+$scope.Asalto=function(){
+	alert("asalto");
+}
 
 });
