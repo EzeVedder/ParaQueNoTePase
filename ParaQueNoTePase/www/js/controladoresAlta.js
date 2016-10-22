4a0df8ce2916e9bb31f41ef49ea16fb6a018b0ef
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-10-22 13:38:31 -0300

    Alta controller

--- ParaQueNoTePase/www/js/controladoresAlta.js
+++ ParaQueNoTePase/www/js/controladoresAlta.js
@@ -1,55 +1,38 @@
 angular.module('alta.controllers', [])
 
-app.controller('controlAlta', function($scope, $http, FileUploader) {
-    $scope.DatoTest="**alta**";
+//app.controller('controlAlta', function($scope, $http, FileUploader) {
+.controller('controlAlta', function($scope, Delitos){
 
     //Inicio las variables
-    $scope.persona = {};
-    $scope.persona.foto = "sinfoto.jpg";
+    $scope.alta = {};
+    $scope.alta.foto = "sinfoto.jpg";
     
-    $scope.uploader = new FileUploader({url:'PHP/upload.php'});
-    $scope.uploader.queueLimit = 1;
-
-
-    $scope.ValidarText = function(name){
-        return !($scope.formAlta[name].$dirty && $scope.formAlta[name].$invalid);
-    }
-
     $scope.Guardar=function(){
-        if($scope.uploader.queue.length>0){
-            console.info($scope.uploader.queue.length);
-            console.info($scope.uploader.queue[0].file.name);
-            $scope.uploader.queue[0].file.name = $scope.persona.dni+".jpg";
-            $scope.persona.foto = $scope.persona.dni+".jpg";
-            $scope.uploader.uploadAll();
-            console.info($scope.uploader.queue[0].file.name);
-        }
 
-        console.log("persona a guardar:");
-        console.log($scope.persona);
+        //Recupero las coordenadas
+        $scope.getLocation();
+        
+        alert("La carga se realizó con éxito");
+        $scope.alta = {};
+        $scope.alta.foto = "sinfoto.jpg";
 
-        //$http.post('PHP/nexo.php', { datos: {accion :"insertar",persona:$scope.persona}})
-        //$http.get('PHP/nexo.php', { params: {accion: "insertar", persona: $scope.persona}})// -> Sin Slim Framework
-
-        //Le tendria que pasar el objeto hecho string como parametro. Para eso lo paso como ruta.
-        //$http.post('ws1/usuario/'+JSON.stringify($scope.persona))
-        $http.post('ws1/usuario/'+"Lautaro")
-        .then(function(respuesta) {         
-            //aca se ejetuca si retorno sin errores         
-            console.log(respuesta.data);
-        },function errorCallback(response) {            
-            //aca se ejecuta cuando hay errores
-            console.log( response);                 
-        });
     }
 
-    /*
-        //--------------------------- FILE-UPLOAD --------------------------------------
-        $scope.cargar = function(){
-            $scope.uploader.uploadAll();
+    $scope.getLocation = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition($scope.showPosition);
+        } else {
+            $scope.alta.lat = "Sin datos";
+            $scope.alta.lon = "Sin datos";
         }
-        $scope.uploader.onCompleteAll = function() {
-            console.info('Se cargo con exito');
-        };
-    */
+    }
+
+    $scope.showPosition = function(position) {
+        setTimeout(function() {
+            $scope.alta.latitud=position.coords.latitude;
+            $scope.alta.longitud=position.coords.longitude;
+            console.info($scope.alta);
+            Delitos.cargar($scope.alta)
+        })
+    }
 });
